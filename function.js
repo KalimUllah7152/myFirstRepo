8eb6d12dacc728380305e449931e6cd01a42e3fc
KalimUllah7152 <mdkalimullah7152@gmail.com>
2022-01-30 11:09:26 +0600

    commit

---
+++ function.js
@@ -0,0 +1,16 @@
+// function declaration
+function startFan(){
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+// call the function
+startFan();
+
+
+
+
+
+
+
+
+
---
+++ recap.js
@@ -0,0 +1,30 @@
+var favoriteBook = 'Four hour week'
+
+// array
+var booksList = ['English', 'Grammer', 'Bangla', 'ICT', 'Arabic'];
+var shoeDogIndex = booksList.indexOf('Grammer');
+booksList[2]= 'ICT'
+
+booksList.push('Urdu')
+booksList.pop()
+// console.log(shoeDogIndex);
+
+// loop
+// while loop
+var i = 0; // loop variable
+while(i < 15){
+    console.log(i);
+    console.log('looping looping looping');
+    i++;
+}
+
+// for loop
+for(var i = 0; i < 15; i++){
+    console.log(i);
+}
+
+
+
+
+
+
